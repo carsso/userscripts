0ab090aecfe61d3b73765b34f56c2bdd0a871494
Germain Carré <germaincarre@yahoo.fr>
2018-02-01 23:12:11 +0100

    changing button to handle window resizing

--- tf1-skip-login-anti-adblock.user.js
+++ tf1-skip-login-anti-adblock.user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name         TF1 login and anti-adblock bypass
-// @version      0.4
+// @version      0.5
 // @description  TF1 login and anti-adblock bypass
 // @author       Carsso
 // @updateURL    https://openuserjs.org/meta/Carsso/TF1_login_and_anti-adblock_bypass.meta.js
@@ -14,29 +14,18 @@
 (function() {
     'use strict';
     window.onload = function () {
-        if(document.querySelector('#zonePlayer')) {
-            for (var i =0; i < 10; i++) {
-                setTimeout(function() {
-                    if(document.querySelector('#program_nav nav ul li.follow button')) {
-                        document.querySelector('#program_nav nav ul li.follow button').remove();
-                        var btn = document.createElement("a");
-                        btn.setAttribute('style', 'color: #fff;');
-                        btn.setAttribute('onclick', "var iframe = document.createElement('iframe');iframe.setAttribute('src', document.querySelector('#zonePlayer').getAttribute('data-src')+'?useHD=1');iframe.setAttribute('class', 'iframe_player');iframe.setAttribute('style', 'width:100%;height:100%;');document.querySelector('body').replaceWith(iframe);document.querySelector('html').setAttribute('style', 'width:100%;height:100%;');");
-                        btn.innerHTML = 'LOGIN/ANTI-ADBLOCK BYPASS';
-                        document.querySelector('#program_nav nav ul li.follow').appendChild(btn);
-                    }
-                    if(document.querySelector('a.large_external_link')) {
-                        document.querySelector('a.large_external_link').remove();
-                        document.querySelector('a.large_external_link').remove();
-                        var btn2 = document.createElement("div");
-                        btn2.setAttribute('class', 'large_external_link');
-                        btn2.setAttribute('style', 'width:auto;padding: 0 14px;');
-                        btn2.setAttribute('onclick', "var iframe = document.createElement('iframe');iframe.setAttribute('src', document.querySelector('#zonePlayer').getAttribute('data-src')+'?useHD=1');iframe.setAttribute('class', 'iframe_player');iframe.setAttribute('style', 'width:100%;height:100%;');document.querySelector('body').replaceWith(iframe);document.querySelector('html').setAttribute('style', 'width:100%;height:100%;');");
-                        btn2.innerHTML = 'LOGIN/ANTI-ADBLOCK BYPASS';
-                        document.querySelector('nav#header_large').insertBefore(btn2, document.querySelector('nav#header_large nav.large_account'));
-                    }
-                }, 2*1000*i);
-            }
+        if(document.querySelector('div#zonePlayer')) {
+            document.querySelector('div#zonePlayer div.needAdBlockDisabled p').remove();
+            document.querySelector('div#zonePlayer div.needSubscribe p').remove();
+            var btn3 = document.createElement("a");
+            btn3.setAttribute('class', 'buttonConnect toConnect');
+            btn3.setAttribute('style', 'width:auto;padding: 0 14px;');
+            btn3.setAttribute('onclick', "var iframe = document.createElement('iframe');iframe.setAttribute('src', document.querySelector('#zonePlayer').getAttribute('data-src')+'?useHD=1');iframe.setAttribute('class', 'iframe_player');iframe.setAttribute('style', 'width:100%;height:100%;');document.querySelector('body').replaceWith(iframe);document.querySelector('html').setAttribute('style', 'width:100%;height:100%;');");
+            btn3.innerHTML = 'LOGIN/ANTI-ADBLOCK BYPASS';
+            var p3 = document.createElement("p");
+            p3.appendChild(btn3);
+            document.querySelector('div#zonePlayer div.needAdBlockDisabled').appendChild(p3);
+            document.querySelector('div#zonePlayer div.needSubscribe').appendChild(p3);
         }
     };
 })();
