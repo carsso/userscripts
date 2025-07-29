a9c67f00aac6aafdbf8dcaa462d430c41874db71
Germain Carré <git@germain.sh>
2025-07-29 14:06:12 +0200

    Enhance vSphere Client v0.3

--- Enhanced-vSphere-Client.user.js
+++ Enhanced-vSphere-Client.user.js
@@ -1,13 +1,13 @@
 // ==UserScript==
-// @namespace     https://openuserjs.org/users/Carsso
 // @name         Enhanced vSphere Client
-// @version      0.2
-// @description  Enhance your vSphere Client - Supports vCenter 7.0.3
+// @namespace    https://openuserjs.org/users/Carsso
+// @version      0.3
+// @description  Enhance your vSphere Client - Supports vCenter 7.0, 8.0
 // @author       Carsso
 // @updateURL    https://openuserjs.org/meta/Carsso/Enhanced_vSphere_Client.meta.js
 // @downloadURL  https://openuserjs.org/install/Carsso/Enhanced_vSphere_Client.user.js
 // @homepageURL  https://github.com/carsso/userscripts
-// @include      *://*/ui/*
+// @match        *://*/ui/*
 // @grant        GM_addStyle
 // @license      MIT
 // ==/UserScript==
@@ -22,7 +22,8 @@
         (new MutationObserver(check)).observe(document, { childList: true, subtree: true });
 
         function check(changes, observer) {
-            if (document.querySelector('vsc-app-main-layout .vui-panel object-navigator-view .inventory-tree vsc-inventory-tree-view')) {
+            //if (document.querySelector('vsc-app-main-layout .vui-panel object-navigator-view .inventory-tree vsc-inventory-tree-view')) {
+            if (document.querySelector('vsc-app-main-layout .vui-panel object-navigator-view')) {
                 observer.disconnect();
                 console.log('Enhancing vSphere Client...');
                 var titleElem = document.querySelector('vsc-app-main-layout .branding .title[data-test-id="vsphere.core.controlcenter.domainView"]')
@@ -44,6 +45,21 @@
                         width: 0.8rem;
                     }
                 `);
+                document.querySelector('.column-switch-wrapper button').dispatchEvent(new Event('click'));
+                setTimeout(function(){
+                    var labels = document.querySelectorAll('.column-switch .switch-content label');
+                    for (let label of labels) {
+                        if (label.innerHTML === "Serveur"
+                            || label.innerHTML === "Server"
+                            || label.innerHTML === "Mise en file d'attente pendant"
+                            || label.innerHTML === "Queued For") {
+                            var input = document.getElementById(label.htmlFor);
+                            input.checked = false;
+                            input.dispatchEvent(new Event('change'));
+                        }
+                    }
+                    document.querySelector('.column-switch-wrapper button').dispatchEvent(new Event('click'));
+                }, 10);
             }
         }
     }
